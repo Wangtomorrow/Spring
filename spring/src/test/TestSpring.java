1a13d998db5d301186badcdfbc415f8de3a1d4ee
Wangtomorrow <914606466@qq.com>
2018-09-11 18:37:00 +0800

    springIOC/DI简单测试

---
+++ spring/src/test/TestSpring.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pojo.Category;
+import pojo.Product;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
+        Category c =(Category) context.getBean("c");
+        System.out.println(c.getName());
+
+        Product product = (Product) context.getBean("p");
+        System.out.println(product.getCategory().getName());
+        System.out.println(product.getName());
+    }
+}
